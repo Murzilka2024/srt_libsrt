fb0b30757b3275e1c00fba364312a53e3f5604c2
Murzilka2024 <isdnvoip@gmail.com>
2024-09-08 05:00:06 +0200

    Обновление скомпилированного файла setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,29 @@ from setuptools.command.install import install
 import shutil
 import os
 
-class CustomInstall(install):
+class CustomInstallCommand(install):
     def run(self):
-        target_dir = os.path.join(self.install_lib, 'srt_libsrt')
-        self.mkpath(target_dir)
-        shutil.copy('srt_libsrt.cpython-37m-x86_64-linux-gnu.so', target_dir)
+        # Выполняем стандартную установку
         install.run(self)
+        
+        # Путь к файлу .so
+        so_file = 'srt_libsrt.cpython-37m-x86_64-linux-gnu.so'
+        # Директория установки
+        target_dir = self.install_lib
+        
+        # Копируем .so файл в директорию установки, если файл существует
+        if os.path.exists(so_file):
+            shutil.copy(so_file, target_dir)
+            print(f"Файл {so_file} успешно скопирован в {target_dir}.")
+        else:
+            print(f"Файл {so_file} не найден!")
 
 setup(
     name='srt_libsrt',
     version='0.1',
-    description='Precompiled SRT library for Python',
-    author='murzilka2024',
     packages=find_packages(),
-    cmdclass={'install': CustomInstall},
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: Linux',
-    ],
+    cmdclass={'install': CustomInstallCommand},
+    package_data={
+        '': ['srt_libsrt.cpython-37m-x86_64-linux-gnu.so'],
+    },
 )
